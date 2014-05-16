e37c972127dcbbe4e71a4f2e151517f92db22f4c
pbertoni <giskard89@gmail.com>
2014-05-16 16:59:52 +0200

    script sistemati definitivamente. hope so

--- scripts/inspect-interno.sh
+++ scripts/inspect-interno.sh
@@ -2,4 +2,4 @@
 
 # il numero 1 finale è la mia videocamera USB. opencv indicizza da 0: quindi un laptop con videocamera integrata DOVREBBE (compatibilità linux..)
 # almeno la videocamera 0. in ogni caso, se opencv non trova quella cercata, fallisce e termina avvisando su stdout.
-./inspect parameters.yml 1
+../inspect ../parameters.yml 0
