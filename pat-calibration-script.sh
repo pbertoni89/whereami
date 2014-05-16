49f3e4ac994d2f182b60f77550a04f3f29e79931
Patrizio Bertoni <giskard89@gmail.com>
2014-05-16 14:43:33 +0200

    Delete pat-calibration-script.sh

--- pat-calibration-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# il numero 1 finale è la mia videocamera USB. opencv indicizza da 0: quindi un laptop con videocamera integrata DOVREBBE (compatibilità linux..)
-# almeno la videocamera 0. in ogni caso, se opencv non trova quella cercata, fallisce e termina avvisando su stdout.
-./calibration -w 9 -h 6 -o parameters.yml -op -oe 1
