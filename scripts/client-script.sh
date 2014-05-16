273b592100387f1cfc1cd17c6d13cf4351d6431b
pbertoni <giskard89@gmail.com>
2014-05-16 16:49:57 +0200

    dovrebbe essere una pulizia generale

--- scripts/client-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# il numero 1 finale è la mia videocamera USB. opencv indicizza da 0: quindi un laptop con videocamera integrata DOVREBBE (compatibilità linux..)
-# almeno la videocamera 0. in ogni caso, se opencv non trova quella cercata, fallisce e termina avvisando su stdout.
-./client 127.0.0.1
